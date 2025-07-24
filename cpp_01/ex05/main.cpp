8c1b23f41be61061149cc31e4329f02eb9c3b0e6
Reem Essameldin <reem.essam.eldin@gmail.com>
2025-07-24 15:43:46 +0200

    ex05 DONE

--- cpp_01/ex05/main.cpp
+++ cpp_01/ex05/main.cpp
@@ -1,12 +1,34 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.cpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: reldahli <reldahli@student.42berlin.de>    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/07/24 15:21:56 by reldahli          #+#    #+#             */
+/*   Updated: 2025/07/24 15:42:22 by reldahli         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include "Harl.hpp"
 #include <iostream>
 
-int main() {
-    Harl harl;
-    harl.complain("DEBUG");
-    harl.complain("INFO");
-    harl.complain("WARNING");
-    harl.complain("ERROR");
-    harl.complain("OTHER");
-    return 0;
+
+int main(int argc, char **argv)
+{
+	Harl	harl;
+	std::string level;
+
+	if (argc != 2)
+	{
+		std::cerr << GREEN <<"Enter a complaint level (DEBUG, INFO, WARNING, ERROR): " << RESET << std::endl;
+		return 1;
+	}
+
+	else
+	{
+		level = argv[1];
+		harl.complain(level);
+	}
+	return 0;
 }
