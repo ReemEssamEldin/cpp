0e41ff711c1e8f536e1985a0491f88cfc7ecd887
Reem Essameldin <reem.essam.eldin@gmail.com>
2025-07-27 02:07:11 +0200

    makefile and bug fix

--- cpp_04/ex01/Animal.cpp
+++ cpp_04/ex01/Animal.cpp
@@ -27,6 +27,10 @@ Animal::~Animal() {
     std::cout << "Animal destructor called" << std::endl;
 }
 
+void Animal::makeSound() const {
+    std::cout << "Animal makes a generic sound" << std::endl;
+}
+
 std::string Animal::getType() const {
     return _type;
 }
